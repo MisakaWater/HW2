c9bf790d8ed17bf890dc0055fa6f20aaf70e7fb7
Misaka <MisakaWater@outlook.com>
2020-05-12 14:18:05 +0800

    update-database:添加AnswerText字段，修改AnswerPaht为AnswerFile

--- HW2/Data/Migrations/20200512054035_updata_addAnswerTextAndChangeAnswerPathToAnswerFile.Designer.cs
+++ HW2/Data/Migrations/20200512054035_updata_addAnswerTextAndChangeAnswerPathToAnswerFile.Designer.cs
@@ -4,14 +4,16 @@ using HW2.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HW2.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200512054035_updata_addAnswerTextAndChangeAnswerPathToAnswerFile")]
+    partial class updata_addAnswerTextAndChangeAnswerPathToAnswerFile
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
