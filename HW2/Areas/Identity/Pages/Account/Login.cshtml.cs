b5a4a7b76303c2be9c7f69134262858dfa800e78
naigv <1961797591@qq.com>
2020-05-12 22:59:25 +0800

    feature:login

--- HW2/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ HW2/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -50,7 +50,7 @@ namespace HW2.Areas.Identity.Pages.Account
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
-            [Display(Name = "忘记密码？")]
+            [Display(Name = "记住密码")]
             public bool RememberMe { get; set; }
         }
 
